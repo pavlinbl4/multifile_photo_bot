f085ab006476b4c9c7afb96b53e19b9941357e43
bigMac <bigMac@bigMac>
2025-03-07 22:03:39 +0300

    проблема была в определении photo_id, на данный момент функция отключена

--- photo_uplolader/photo_id.py
+++ photo_uplolader/photo_id.py
@@ -1,9 +1,12 @@
 import re
+from loguru import logger
 
 
 def extract_photo_id(current_url):
     pattern = r'KSP_\d+_\d+'
-    return re.findall(pattern, current_url)[0]
+    photo_id = re.findall(pattern, current_url)[0]
+    logger.info(f"Photo ID: {photo_id}")
+    return photo_id
 
 
 
