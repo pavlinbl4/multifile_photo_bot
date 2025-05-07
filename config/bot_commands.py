6a31f9151706c454e0be4a6bb68054a91d0c8532
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-05-07 22:30:17 +0300

    added finish command

--- config/bot_commands.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import BotCommand
-
-# При необходимости можно определить новые команды
-COMMANDS = [
-    BotCommand(command="start", description="Запустить бота"),
-    BotCommand(command="help", description="Показать справку"),
-    BotCommand(command="add_image", description="Загрузить изображение"),
-    BotCommand(command="cancel", description="Отменить текущую операцию")
-]
