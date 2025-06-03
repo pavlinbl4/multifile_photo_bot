1bc6bddacc1a192c74555c0b6f9553ac7b684f0d
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-06-03 09:03:47 +0300

    на данном этапе после нескольких тестирований бот работает хорошо

---
+++ main.py
@@ -0,0 +1,41 @@
+import asyncio
+from loguru import logger
+
+from core.bot import setup_bot, setup_bot_commands, register_all_handlers
+
+from services.selenium_service import selenium_worker
+from core.bot import setup_bot, register_all_handlers, setup_bot_commands
+
+async def main():
+    # Настраиваем логирование
+    bot, dp = setup_bot()
+
+    # Регистрация обработчиков и команд
+    register_all_handlers(dp)
+
+    # Устанавливаем команды бота
+    await setup_bot_commands(bot)
+
+    # Запускаем воркер для обработки задач Selenium
+    selenium_task = asyncio.create_task(selenium_worker(bot))
+
+    try:
+        # Запускаем бота
+        await dp.start_polling(bot)
+    finally:
+        # Закрываем соединения
+        await bot.session.close()
+        # Останавливаем воркер корректно
+        selenium_task.cancel()
+
+        try:
+            await selenium_task
+        except asyncio.CancelledError:
+            pass
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.info("Bot stopped!")
