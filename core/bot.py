103dadc2770df176aa793025b96ec33e3000b1f4
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-06-03 09:16:29 +0300

    added loggin for token type

--- core/bot.py
+++ core/bot.py
@@ -19,6 +19,7 @@ def setup_bot():
     storage = MemoryStorage()
 
     # Создаем объекты бота и диспетчера
+    logger.info(f'Token type: {type(TOKEN)}')
     bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
 
