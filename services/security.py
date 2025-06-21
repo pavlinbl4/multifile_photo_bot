72f060848ca4b5f0904eb2b327900a7d33cac15c
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-06-21 21:30:07 +0300

    Updated to PEP8

--- services/security.py
+++ services/security.py
@@ -1,8 +1,10 @@
 from config.settings import ALLOWED_USER_NAMES
 
+
 def is_user_allowed(username):
     """Проверяет, разрешен ли доступ пользователю"""
     return username in ALLOWED_USER_NAMES
+
 
 def log_unauthorized_access(user):
     """Логирует попытки неавторизованного доступа"""
