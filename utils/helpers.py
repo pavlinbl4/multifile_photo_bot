ed27cc4baa5825bb7fc9ae1ab1cc16b4c2a9acd7
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-06-21 21:31:58 +0300

    Updated to PEP8

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
+
 def create_dir(dir_name):
     """Создает директорию, если она не существует"""
     directory = Path(dir_name)
     directory.mkdir(exist_ok=True, parents=True)
     return str(directory.absolute())
+
 
 def is_allowed_file_type(mime_type, allowed_types):
     """Проверяет, является ли тип файла разрешенным"""
