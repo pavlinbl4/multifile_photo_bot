d83e7d1e9662435dfb7fdf46a1e0ac70e61234f0
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-06-21 21:32:33 +0300

    Updated to PEP8

--- utils/photo_id.py
+++ utils/photo_id.py
@@ -1,7 +1,7 @@
 import re
 from loguru import logger
-logger.add("../photo_uploader.log", format="{time} {level} {message}", level="DEBUG")
 
+logger.add("../photo_uploader.log", format="{time} {level} {message}", level="DEBUG")
 
 
 def extract_photo_id(current_url):
@@ -9,7 +9,3 @@ def extract_photo_id(current_url):
     photo_id = re.findall(pattern, current_url)[0]
     logger.debug(f"Photo ID: {photo_id}")
     return photo_id
-
-
-
-
