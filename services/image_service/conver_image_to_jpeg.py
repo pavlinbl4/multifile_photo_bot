2d42e66b24a815b5833d58f4f73d04a3bf5a6eec
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-08-29 21:57:44 +0300

    добавил jpeg в список конвертируемых файлов

--- services/image_service/conver_image_to_jpeg.py
+++ services/image_service/conver_image_to_jpeg.py
@@ -2,9 +2,11 @@ from PIL import Image
 import os
 from loguru import logger
 
+from services.file_service import delete_not_jpeg
+
 
 def convert_image_to_jpeg(path_to_file: str):
-    supported_extensions = ['bmp', 'png', 'gif', 'tiff', 'webp']  # Поддерживаемые расширения
+    supported_extensions = ['bmp', 'png', 'gif', 'tiff', 'webp', 'jpeg', 'jpg']  # Поддерживаемые расширения
     jpeg_quality = 70  # Качество JPEG, от 1 (худшее) до 95 (лучшее)
 
     file_ext = os.path.splitext(path_to_file)[1][1:].lower()  # Получаем расширение файла без точки
@@ -17,7 +19,8 @@ def convert_image_to_jpeg(path_to_file: str):
                 path_to_jpeg_file = path_to_file.replace(file_ext, 'jpg')
                 img.save(path_to_jpeg_file, 'JPEG', quality=jpeg_quality)
                 logger.info("conversion successful")
-                os.remove(path_to_file)
+                # os.remove(path_to_file)
+                delete_not_jpeg(path_to_file)
                 logger.info("initial file deleted")
         except Exception as e:
             logger.error(f"conversion error : {e}")
