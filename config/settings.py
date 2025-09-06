b06a7ebef943874458c914fd15cdfd3c30ec483a
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-09-06 15:39:53 +0300

    working version

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,6 @@ ALLOWED_FILE_TYPES = {'image/jpeg', 'image/png', 'image/x-tiff'}
 MIN_CREDIT_LENGTH = 3
 
 # Настройки логирования
-# LOG_FILE = BASE_DIR / "photo_uploader.log"
 LOG_FILE = BASE_DIR / "photo_uploader.log"
 LOG_LEVEL = "INFO"
 LOG_FORMAT = "{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}"
