16c7e02c013b678b3e0419a160632b3ef0ef8d0f
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-09-06 15:40:48 +0300

    added new state

--- core/states.py
+++ core/states.py
@@ -1,5 +1,6 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class UploadForm(StatesGroup):
+    select_shooter = State() # select shot inner id
     add_credit = State()  # Состояние ожидания ввода image credit
     add_file = State()    # Состояние ожидания добавления файла
